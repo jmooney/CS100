d54717b7ea139e2f703f0d986298794e9685b183
John Mooney <jmooneycs@gmail.com>
2013-02-20 15:17:47 -0500

    Added gitignore, modified GeometryPrimitives1

--- Math/GeometryPrimitives.py
+++ Math/GeometryPrimitives.py
@@ -24,26 +24,16 @@ from Vector import XAxisVector
 from tools import getDictValue
 
 
-#------------------------------------------------------#
-
-class GeometryPrimitive(Transformable):
-	pass
-
-
 #--------------------------------------------------------#
 	
-class GeometryRect(GeometryPrimitive):
+class GeometryRect(Transformable):
 
 	def __initVars__(self, **kwArgs):
 		super().__initVars__(**kwArgs)
 		
-		self._width = w = getDictValue(kwArgs, 1.0, ['w', 'width'])
-		self._height = h = getDictValue(kwArgs, 1.0, ['h', 'height'])
-		
-		#	A diagonal crossing from the center to the top-right
-		self._diagonal 		= vec(w/2, h/2)
-		self._diagonalRot 	= self._diagonal.getAngleTo(XAxisVector)
-		self._dLength 		= self._diagonal.length()
+		self._width 	= w = getDictValue(kwArgs, 1.0, ['w', 'width'])
+		self._height 	= h = getDictValue(kwArgs, 1.0, ['h', 'height'])
+		self._diagonal 	= vec(w/2, h/2)
 		
 		
 	''''''''''''''''''''''''''''''''''''''
@@ -54,20 +44,21 @@ class GeometryRect(GeometryPrimitive):
 	
 	def _onTranslation(self, dif):
 		super()._onTranslation(dif)
+	
 	def _onRotation(self, dif):
 		super()._onRotation(dif)
-		dr = self._worldRot+self._diagonalRot
-
-		self._diagonal.x = math.cos(dr)*self._dLength
-		self._diagonal.y = math.sin(dr)*self._dLength
+		d = self._diagonal
+		d.x = d.x*math.cos(dif) - d.y*math.sin(dif)
+		d.y = d.x*math.sin(dif) + d.y*math.cos(dif)
+		
 	def _onScale(self, dif):
 		super()._onScale(dif)
 		self._diagonal 	*= dif
-		self._dLength	*= dif
+
 
 #-----------------------------------------------#
 
-class GeometryEllipse(GeometryPrimitive):
+class GeometryEllipse(Transformable):
 
 	def __initVars__(self, **kwArgs):
 		super().__initVars__(**kwArgs)
@@ -108,7 +99,7 @@ class GeometryEllipse(GeometryPrimitive):
 		
 #------------------------------------------------#
 
-class GeometryLine(GeometryPrimitive):
+class GeometryLine(Transformable):
 
 	def __initVars__(self, **kwArgs):
 		super().__initVars__(**kwArgs)
